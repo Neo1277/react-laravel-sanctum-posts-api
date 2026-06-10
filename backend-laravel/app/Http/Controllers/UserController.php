7a27f874c4ffcf7d46bcab99e2160d38bbeef8a1
Marlon Meneses <marlon-armando@hotmail.com>
2026-06-10 14:27:59 -0500

    Add PHPDoc for UserController

--- backend-laravel/app/Http/Controllers/UserController.php
+++ backend-laravel/app/Http/Controllers/UserController.php
@@ -9,15 +9,42 @@ use App\Services\UserCrudService;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
+/**
+ * UserController
+ *
+ * Handles user management operations.
+ *
+ * Responsibilities:
+ * - List users
+ * - Create users
+ * - Retrieve a specific user
+ * - Update user information
+ * - Delete users
+ *
+ * Access to these endpoints is typically restricted to administrators.
+ */
 class UserController extends Controller
 {
+    /**
+     * User service instance.
+     */    
     protected UserCrudService $userService;
 
+    /**
+     * Create a new controller instance.
+     *
+     * @param UserCrudService $userService Service responsible for user management.
+     */    
     public function __construct(UserCrudService $userService)
     {
         $this->userService = $userService;
     }
 
+    /**
+     * Display a paginated listing of users.
+     *
+     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
+     */    
     public function index()
     {
         $posts = $this->userService
@@ -27,6 +54,14 @@ class UserController extends Controller
         return UserCrudResource::collection($posts);
     }
 
+    /**
+     * Store a newly created user.
+     *
+     * Creates the user, hashes the password, and assigns the selected role.
+     *
+     * @param StoreUserRequest $request Validated user creation request.
+     * @return \Illuminate\Http\JsonResponse
+     */    
     public function store(StoreUserRequest $request)
     {
         $user = $this->userService->createUser($request->validated());
@@ -40,6 +75,12 @@ class UserController extends Controller
         ])->setStatusCode(Response::HTTP_CREATED);
     }
 
+    /**
+     * Display the specified user.
+     *
+     * @param int $id User identifier.
+     * @return \Illuminate\Http\JsonResponse
+     */    
     public function show($id)
     {
         $user = $this->userService->getUserById($id);
@@ -55,6 +96,15 @@ class UserController extends Controller
         ]);
     }
 
+    /**
+     * Update the specified user.
+     *
+     * Updates user information and synchronizes roles when provided.
+     *
+     * @param UpdateUserRequest $request Validated user update request.
+     * @param int $id User identifier.
+     * @return \Illuminate\Http\JsonResponse
+     */    
     public function update(UpdateUserRequest $request, $id)
     {
         $user = $this->userService->updateUser($id, $request->validated());
@@ -68,6 +118,15 @@ class UserController extends Controller
         ]);
     }
 
+    /**
+     * Remove the specified user.
+     *
+     * Prevents administrators from deleting their own account.
+     *
+     * @param Request $request Current authenticated request.
+     * @param int $id User identifier.
+     * @return \Illuminate\Http\JsonResponse
+     */    
     public function destroy(Request $request, $id)
     {
         $this->userService->deleteUser($id, $request->user()->id);
